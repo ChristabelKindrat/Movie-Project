b62d2e08d0ce5f56577a02dd9661dbd707dc1f2b
ChristabelKindrat <christabelkril@gmail.com>
2022-08-13 13:26:53 +0300

    MovieProject

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
 
 
 function UserInfo() {
+
+    const {register, handleSubmit} = useForm();
+
+
     return (
         <div className={'UserInfoWrap'}>
             <div className={'UserInfoAbout'}>
-                <p className={'UserInfoName'}>Name Surname</p>
-                <p className={'UserInfoGmail'}>useradress@gmail.com</p>
+
+                <form className={'UserForm'} onSubmit={handleSubmit((obj)=>{
+                    console.log(obj)
+                //    !Push to API Server
+
+                })}>
+
+                    <input className={'UserInfoName'} type={"text"} placeholder={'Username'} {...register('username')} />
+                    <input className={'UserInfoPassword'} type={'text'} placeholder={'Password'} {...register('password')}/>
+                    <button>Register</button>
+                </form>
+
             </div>
-            <img className={'UserInfoImg'} src={'https://thumbs.dreamstime.com/b/default-avatar-profile-flat-icon-social-media-user-vector-portrait-unknown-human-image-default-avatar-profile-flat-icon-184330869.jpg'}/>
+            <img className={'UserInfoImg'}
+                 src={'https://thumbs.dreamstime.com/b/default-avatar-profile-flat-icon-social-media-user-vector-portrait-unknown-human-image-default-avatar-profile-flat-icon-184330869.jpg'}/>
         </div>
     );
 }
